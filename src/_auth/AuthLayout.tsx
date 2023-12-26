121fd7b7646e859f7cfa7ee0ce27a9b4ff0c4fce
Meet <meetmendapara09@gmail.com>
2023-12-26 13:22:51 +0530

    changes in AuthLayout.tsx

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,7 @@
-import { Outlet , Navigate } from 'react-router-dom';
-const AuthLayout = () => {
-  const isAuthenticated = false;
+import { Outlet , Navigate } from "react-router-dom";
+import { useUserContext } from "@/context/AuthContext";
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
   return (
     <>
       {isAuthenticated ? (
@@ -10,16 +11,13 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-content items-center flex-col py-10">
             <Outlet />
           </section>
-
           <img 
               src="/assets/images/side-img.svg"
               alt="logo"
-              className="hidden xl:block h-screen w-1/2 object-cover bg-none-repeat "/>
-
+              className="hidden xl:block h-screen w-1/2 object-cover bg-none-repeat "
+              />
           </>
         )}
     </>
-  )
+  );
 }
-
-export default AuthLayout
