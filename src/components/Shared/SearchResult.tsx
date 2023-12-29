ff50793d3d84ecf75b59a78e7049b363287e9a2c
Meet <meetmendapara09@gmail.com>
2023-12-29 12:27:26 +0530

    changes some path files

---
+++ src/components/Shared/SearchResult.tsx
@@ -0,0 +1,22 @@
+import { Models } from 'appwrite';
+import Loader from '@/components/shared//Loader';
+import  GridPostList  from '@/components/shared//GridPostList';
+
+type SearchResultsProps = {
+    isSearchFetching: boolean;
+    searchedPosts: Models.Document[];
+}
+
+const SearchResults = ({ isSearchFetching, searchedPosts }:
+SearchResultsProps) => {
+    if(isSearchFetching) return <Loader />
+    if (searchedPosts && searchedPosts.documents.length > 0) return 
+    return (
+        <GridPostList posts={searchedPosts.documents} />
+    }
+    return (
+    <p classname="text-light-4 mt-10 text-center w-4">NO results found</p>
+    )
+}
+
+export default SearchResults
