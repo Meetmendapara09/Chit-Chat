f78b6a2f6832ba95ca8d81e0302dfc63efaaa084
Meet <meetmendapara09@gmail.com>
2024-01-13 14:23:49 +0530

    added some important file

---
+++ src/_root/pages/about.js
@@ -0,0 +1,14 @@
+export default () => {
+    return (
+      <a
+        href=""
+        onClick={(e) => {
+          window.history.back()
+          e.preventDefault()
+        }}
+      >
+        Back
+      </a>
+    )
+  }
+  
