3cc487a8c58e3276b5031cf549639f0d1dc94ecf
Meet <meetmendapara09@gmail.com>
2024-01-13 16:14:59 +0530

    update the tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme';
 
-module.exports = {
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -44,7 +44,7 @@ module.exports = {
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
-
+        custom: defaultTheme.fontFamily.sans,
       },
       keyframes: {
         'accordion-down': {
